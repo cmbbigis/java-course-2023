e0eea621bf93853416bdc94625e801e601b72d96
cmbbigis <gavrilov.nikita@urfu.me>
2023-12-14 18:34:04 +0500

    refactor

--- src/test/java/edu/hw7/Task3Test.java
+++ src/test/java/edu/hw7/Task3Test.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.Collections;
 
+import java.util.Collections;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task3Test {
@@ -18,6 +23,26 @@ public class Task3Test {
         person2 = new Task3.Person(2, "Bob", "456 Avenue", "222-2222");
         db.add(person1);
         db.add(person2);
+    }
+
+    @Test
+    public void testParallelOperations() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        executorService.submit(() -> assertThat(db.findByName("Alice")).isEqualTo(Collections.singletonList(person1)));
+        executorService.submit(() -> assertThat(db.findByName("Bob")).isEqualTo(Collections.singletonList(person2)));
+        executorService.submit(() -> assertThat(db.findByAddress("123 Street")).isEqualTo(Collections.singletonList(person1)));
+        executorService.submit(() -> assertThat(db.findByAddress("456 Avenue")).isEqualTo(Collections.singletonList(person2)));
+        executorService.submit(() -> assertThat(db.findByPhone("111-1111")).isEqualTo(Collections.singletonList(person1)));
+        executorService.submit(() -> assertThat(db.findByPhone("222-2222")).isEqualTo(Collections.singletonList(person2)));
+
+        executorService.submit(() -> db.delete(1));
+        executorService.submit(() -> assertThat(db.findByName("Alice").isEmpty()).isTrue());
+        executorService.submit(() -> assertThat(db.findByAddress("123 Street").isEmpty()).isTrue());
+        executorService.submit(() -> assertThat(db.findByPhone("111-1111").isEmpty()).isTrue());
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
     }
 
     @Test
