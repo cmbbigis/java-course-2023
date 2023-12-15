5a909c68dedf8fd34a97843e88cdf8acc7f56493
cmbbigis <gavrilov.nikita@urfu.me>
2023-12-15 14:18:15 +0500

    done 1-2

--- src/test/java/edu/hw11/Task2Test.java
+++ src/test/java/edu/hw11/Task2Test.java
@@ -3,6 +3,7 @@ package edu.hw11;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.FixedValue;
 import net.bytebuddy.matcher.ElementMatchers;
 import net.bytebuddy.pool.TypePool;
@@ -11,20 +12,20 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task2Test {
     @Test
-    void testToString() throws IllegalAccessException, InstantiationException {
+    void testRedefine() throws IllegalAccessException, InstantiationException {
         TypeDescription typeDescription = TypePool.Default.ofSystemLoader()
             .describe("edu.hw11.ArithmeticUtils")
             .resolve();
         Class<?> dynamicType = new ByteBuddy()
             .redefine(typeDescription, ClassFileLocator.ForClassLoader.ofSystemLoader())
             .method(ElementMatchers.named("sum"))
-            .intercept(FixedValue.value(1000))
+            .intercept(FixedValue.value(9999))
             .make()
-            .load(getClass().getClassLoader())
+            .load(ClassLoader.getSystemClassLoader(), ClassLoadingStrategy.Default.INJECTION)
             .getLoaded();
 
         ArithmeticUtils instance = (ArithmeticUtils) dynamicType.newInstance();
 
-        assertThat(instance.sum(1,1)).isEqualTo(1000);
+        assertThat(instance.sum(1,1)).isEqualTo(9999);
     }
 }
