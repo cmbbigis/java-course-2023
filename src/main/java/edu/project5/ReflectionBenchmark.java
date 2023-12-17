071675f309e67cfcb235084359acf381ccddbba4
cmbbigis <gavrilov.nikita@urfu.me>
2023-12-17 18:06:01 +0500

    init

---
+++ src/main/java/edu/project5/ReflectionBenchmark.java
@@ -0,0 +1,83 @@
+package edu.project5;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.lang.invoke.*;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+@State(Scope.Thread)
+public class ReflectionBenchmark {
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+            .include(ReflectionBenchmark.class.getSimpleName())
+            .shouldFailOnError(true)
+            .shouldDoGC(true)
+            .mode(Mode.AverageTime)
+            .timeUnit(TimeUnit.NANOSECONDS)
+            .forks(1)
+            .warmupForks(1)
+            .warmupIterations(1)
+            .warmupTime(TimeValue.seconds(5))
+            .measurementIterations(1)
+            .measurementTime(TimeValue.seconds(5))
+            .build();
+
+        new Runner(options).run();
+    }
+
+    record Student(String name, String surname) {
+    }
+
+    private Student student;
+    private Method method;
+    private MethodHandle methodHandle;
+    private Function<Student, String> function;
+
+    @Setup
+    public void setup() throws NoSuchMethodException, IllegalAccessException, LambdaConversionException {
+        student = new Student("Alexander", "Biryukov");
+        method = Student.class.getMethod("name");
+
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        methodHandle = lookup.unreflect(method);
+
+        MethodType invokedType = MethodType.methodType(Function.class);
+        MethodType methodType = MethodType.methodType(String.class, Student.class);
+        MethodHandle factory = LambdaMetafactory.metafactory(lookup, "apply",
+            invokedType, methodType, methodHandle, methodType).getTarget();
+
+        function = (Function<Student, String>) factory.invokeExact();
+    }
+
+    @Benchmark
+    public void directAccess(Blackhole bh) {
+        String name = student.name();
+        bh.consume(name);
+    }
+
+    @Benchmark
+    public void reflection(Blackhole bh) throws Exception {
+        String name = (String) method.invoke(student);
+        bh.consume(name);
+    }
+
+    @Benchmark
+    public void methodHandle(Blackhole bh) throws Throwable {
+        String name = (String) methodHandle.invokeExact(student);
+        bh.consume(name);
+    }
+
+    @Benchmark
+    public void lambdaMetafactory(Blackhole bh) {
+        String name = function.apply(student);
+        bh.consume(name);
+    }
+}
