1e8281d7de5b3aa228abeb74332ce9d775fad096
cmbbigis <gavrilov.nikita@urfu.me>
2023-12-17 19:49:29 +0500

    done

--- src/main/java/edu/project5/ReflectionBenchmark.java
+++ src/main/java/edu/project5/ReflectionBenchmark.java
@@ -1,6 +1,17 @@
 package edu.project5;
 
-import org.openjdk.jmh.annotations.*;
+import java.lang.invoke.LambdaMetafactory;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -8,13 +19,17 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
 
-import java.lang.invoke.*;
-import java.lang.reflect.Method;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-
 @State(Scope.Thread)
 public class ReflectionBenchmark {
+    private static final int WARMUP_TIME = 5;
+    private static final int MEASUREMENT_TIME = 5;
+    private static final String METHOD_NAME = "name";
+    private Student student;
+    private Method method;
+    private MethodHandle methodHandle;
+    private Function<Student, String> function;
+
+    @SuppressWarnings("checkstyle:UncommentedMain")
     public static void main(String[] args) throws RunnerException {
         Options options = new OptionsBuilder()
             .include(ReflectionBenchmark.class.getSimpleName())
@@ -25,34 +40,30 @@ public class ReflectionBenchmark {
             .forks(1)
             .warmupForks(1)
             .warmupIterations(1)
-            .warmupTime(TimeValue.seconds(5))
+            .warmupTime(TimeValue.seconds(WARMUP_TIME))
             .measurementIterations(1)
-            .measurementTime(TimeValue.seconds(5))
+            .measurementTime(TimeValue.seconds(MEASUREMENT_TIME))
             .build();
 
         new Runner(options).run();
     }
 
-    record Student(String name, String surname) {
-    }
-
-    private Student student;
-    private Method method;
-    private MethodHandle methodHandle;
-    private Function<Student, String> function;
-
     @Setup
     public void setup() throws Throwable {
         student = new Student("Alexander", "Biryukov");
-        method = Student.class.getMethod("name");
+        method = Student.class.getMethod(METHOD_NAME);
 
-        MethodHandles.Lookup lookup = MethodHandles.lookup();
-        methodHandle = lookup.unreflect(method);
-
-        MethodType invokedType = MethodType.methodType(Function.class);
-        MethodType methodType = MethodType.methodType(String.class, Student.class);
-        MethodHandle factory = LambdaMetafactory.metafactory(lookup, "apply",
-            invokedType, methodType, methodHandle, methodType).getTarget();
+        MethodHandles.Lookup publicLookup = MethodHandles.lookup();
+        MethodType methodType = MethodType.methodType(String.class);
+        methodHandle = publicLookup.findVirtual(Student.class, METHOD_NAME, methodType);
+        MethodHandle factory = LambdaMetafactory.metafactory(
+            publicLookup,
+            "apply",
+            MethodType.methodType(Function.class),
+            MethodType.methodType(Object.class, Object.class),
+            methodHandle,
+            MethodType.methodType(String.class, Student.class)
+        ).getTarget();
 
         function = (Function<Student, String>) factory.invokeExact();
     }
@@ -79,5 +90,8 @@ public class ReflectionBenchmark {
     public void lambdaMetafactory(Blackhole bh) {
         String name = function.apply(student);
         bh.consume(name);
+    }
+
+    private record Student(String name, String surname) {
     }
 }
